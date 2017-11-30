373fb9903a02f28de61f100d1485534150cbdd5d
uestcchicken <844230567@qq.com>
2017-11-30 15:00:12 +0800

    300 training

--- mtyun.py
+++ mtyun.py
@@ -9,7 +9,7 @@ LEARNING_RATE = 1e-4
 BATCH_SIZE = 1
 EPOCH = 500
 START_NUM = 1
-MAX_NUM = 40
+MAX_NUM = 300
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type = str, default = '', help = 'input')
@@ -19,20 +19,21 @@ flags, _ = parser.parse_known_args()
 DEN_PATH = flags.data_dir + 'den/'
 IMG_PATH = flags.data_dir + 'img/'
 
+
 def loadnp(f):
-    print('loadnp for ', f)
+    #print('loadnp for ', f)
     with tf.gfile.Open(f, 'rb') as rf:
         content = rf.read()
     
     data = content.split('\n')
     data = [l.split() for l in data[:-1]]
     data = [list(map(float, l)) for l in data]
-    data = [list(map(int, l)) for l in data]
     
-    print(len(data))
-    print(len(data[0]))
+    #print(len(data))
+    #print(len(data[0]))
     n = np.array(data)
-    print(n.shape)
+    #print(n.shape)
+    #print(data[0])
     return n
     
 
@@ -49,14 +50,14 @@ class net:
         self.pre_sum = tf.reduce_sum(self.y_pre)
         self.MAE = tf.abs(self.act_sum - self.pre_sum)
 
-        self.train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(self.loss)
+        self.train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(self.loss)
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             self.train(sess)
 
             #saver = tf.train.Saver()
-            #saver.save(sess, 'model/model.ckpt')
+            #saver.save(sess, OUT_PATH + 'model.ckpt')
 
     def conv2d(self, x, w):
         return tf.nn.conv2d(x, w, strides = [1, 1, 1, 1], padding = 'SAME')
@@ -65,38 +66,96 @@ class net:
         return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 
     def inf(self, x):
+        '''
+        s net ###########################################################
+        '''
+        w_conv1_1 = tf.get_variable('w_conv1_1', [5, 5, 1, 24])
+        b_conv1_1 = tf.get_variable('b_conv1_1', [24])
+        h_conv1_1 = tf.nn.relu(self.conv2d(x, w_conv1_1) + b_conv1_1)
 
-        w_conv1 = tf.get_variable('w_conv1', [5, 5, 1, 24])
-        b_conv1 = tf.get_variable('b_conv1', [24])
-        h_conv1 = tf.nn.relu(self.conv2d(x, w_conv1) + b_conv1)
+        h_pool1_1 = self.max_pool_2x2(h_conv1_1)
 
-        h_pool1 = self.max_pool_2x2(h_conv1)
+        w_conv2_1 = tf.get_variable('w_conv2_1', [3, 3, 24, 48])
+        b_conv2_1 = tf.get_variable('b_conv2_1', [48])
+        h_conv2_1 = tf.nn.relu(self.conv2d(h_pool1_1, w_conv2_1) + b_conv2_1)
 
-        w_conv2 = tf.get_variable('w_conv2', [3, 3, 24, 48])
-        b_conv2 = tf.get_variable('b_conv2', [48])
-        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, w_conv2) + b_conv2)
+        h_pool2_1 = self.max_pool_2x2(h_conv2_1)
 
-        h_pool2 = self.max_pool_2x2(h_conv2)
+        w_conv3_1 = tf.get_variable('w_conv3_1', [3, 3, 48, 24])
+        b_conv3_1 = tf.get_variable('b_conv3_1', [24])
+        h_conv3_1 = tf.nn.relu(self.conv2d(h_pool2_1, w_conv3_1) + b_conv3_1)
 
-        w_conv3 = tf.get_variable('w_conv3', [3, 3, 48, 24])
-        b_conv3 = tf.get_variable('b_conv3', [24])
-        h_conv3 = tf.nn.relu(self.conv2d(h_pool2, w_conv3) + b_conv3)
+        w_conv4_1 = tf.get_variable('w_conv4_1', [3, 3, 24, 12])
+        b_conv4_1 = tf.get_variable('b_conv4_1', [12])
+        h_conv4_1 = tf.nn.relu(self.conv2d(h_conv3_1, w_conv4_1) + b_conv4_1)
+        '''
+        m net ###########################################################
+        '''
+        
+        w_conv1_2 = tf.get_variable('w_conv1_2', [7, 7, 1, 20])
+        b_conv1_2 = tf.get_variable('b_conv1_2', [20])
+        h_conv1_2 = tf.nn.relu(self.conv2d(x, w_conv1_2) + b_conv1_2)
 
-        w_conv4 = tf.get_variable('w_conv4', [3, 3, 24, 12])
-        b_conv4 = tf.get_variable('b_conv4', [12])
-        h_conv4 = tf.nn.relu(self.conv2d(h_conv3, w_conv4) + b_conv4)
+        h_pool1_2 = self.max_pool_2x2(h_conv1_2)
 
-        w_conv5 = tf.get_variable('w_conv5', [1, 1, 12, 1])
+        w_conv2_2 = tf.get_variable('w_conv2_2', [5, 5, 20, 40])
+        b_conv2_2 = tf.get_variable('b_conv2_2', [40])
+        h_conv2_2 = tf.nn.relu(self.conv2d(h_pool1_2, w_conv2_2) + b_conv2_2)
+
+        h_pool2_2 = self.max_pool_2x2(h_conv2_2)
+
+        w_conv3_2 = tf.get_variable('w_conv3_2', [5, 5, 40, 20])
+        b_conv3_2 = tf.get_variable('b_conv3_2', [20])
+        h_conv3_2 = tf.nn.relu(self.conv2d(h_pool2_2, w_conv3_2) + b_conv3_2)
+
+        w_conv4_2 = tf.get_variable('w_conv4_2', [5, 5, 20, 10])
+        b_conv4_2 = tf.get_variable('b_conv4_2', [10])
+        h_conv4_2 = tf.nn.relu(self.conv2d(h_conv3_2, w_conv4_2) + b_conv4_2)
+        
+        
+        '''
+        l net ###########################################################
+        '''
+        w_conv1_3 = tf.get_variable('w_conv1_3', [9, 9, 1, 16])
+        b_conv1_3 = tf.get_variable('b_conv1_3', [16])
+        h_conv1_3 = tf.nn.relu(self.conv2d(x, w_conv1_3) + b_conv1_3)
+
+        h_pool1_3 = self.max_pool_2x2(h_conv1_3)
+
+        w_conv2_3 = tf.get_variable('w_conv2_3', [7, 7, 16, 32])
+        b_conv2_3 = tf.get_variable('b_conv2_3', [32])
+        h_conv2_3 = tf.nn.relu(self.conv2d(h_pool1_3, w_conv2_3) + b_conv2_3)
+
+        h_pool2_3 = self.max_pool_2x2(h_conv2_3)
+
+        w_conv3_3 = tf.get_variable('w_conv3_3', [7, 7, 32, 16])
+        b_conv3_3 = tf.get_variable('b_conv3_3', [16])
+        h_conv3_3 = tf.nn.relu(self.conv2d(h_pool2_3, w_conv3_3) + b_conv3_3)
+
+        w_conv4_3 = tf.get_variable('w_conv4_3', [7, 7, 16, 8])
+        b_conv4_3 = tf.get_variable('b_conv4_3', [8])
+        h_conv4_3 = tf.nn.relu(self.conv2d(h_conv3_3, w_conv4_3) + b_conv4_3)
+        
+
+        '''
+        merge ###########################################################
+        '''
+        h_conv4_merge = tf.concat([h_conv4_1, h_conv4_2, h_conv4_3], 3)
+        
+        
+        
+        w_conv5 = tf.get_variable('w_conv5', [1, 1, 30, 1])
         b_conv5 = tf.get_variable('b_conv5', [1])
-        h_conv5 = tf.nn.relu(self.conv2d(h_conv4, w_conv5) + b_conv5)
-
-
+        #h_conv5 = tf.nn.relu(self.conv2d(h_conv4_merge, w_conv5) + b_conv5)
+        h_conv5 = self.conv2d(h_conv4_merge, w_conv5) + b_conv5
+        
+        
         y_pre = h_conv5
 
         return y_pre
 
     def data_pre(self, img_num):
-        print('data_pre function start.')
+        #print('data_pre function start.')
         den = loadnp(DEN_PATH + 'DEN_' + str(img_num) + '.txt')
         img = loadnp(IMG_PATH + 'IMG_' + str(img_num) + '.txt')
 
@@ -110,18 +169,18 @@ class net:
         
         ceilx = math.ceil(den.shape[0] / 4)
         ceily = math.ceil(den.shape[1] / 4)
-        print('ceil: ', ceilx, 'x', ceily)
-        print('intceil: ', int(ceilx), 'x', int(ceily))
+        #print('ceil: ', ceilx, 'x', ceily)
+        #print('intceil: ', int(ceilx), 'x', int(ceily))
         den_quarter = np.zeros((int(ceilx), int(ceily)))
         
         for i in range(den_quarter.shape[0]):
             for j in range(den_quarter.shape[1]):
                 den_quarter[i, j] = np.sum(den[i * 4: i * 4 + 4, j * 4: j * 4 + 4])
 
-        print('density shape: ', den.shape)
-        print('density sum: ', np.sum(den))
-        print('density quarter shape', den_quarter.shape)
-        print('density quarter sum: ', np.sum(den_quarter))
+        #print('density shape: ', den.shape)
+        #print('density sum: ', np.sum(den))
+        #print('density quarter shape', den_quarter.shape)
+        #print('density quarter sum: ', np.sum(den_quarter))
 
         den_quarter = np.reshape(den_quarter, (1, den_quarter.shape[0], den_quarter.shape[1], 1))
 
@@ -199,6 +258,9 @@ class net:
                 print('loss: ', l)
                 print('act sum: ', act_s)
                 print('pre sum: ', pre_s)
+                print('mae: ', m)
+                
+                
                 
                 
                 epoch_mae += m
